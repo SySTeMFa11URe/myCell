d2d63bcd9389468294cc7de2d9f47040378497ea
SySTeMFa11URe <jam.morford@gmail.com>
2017-11-05 17:16:36 -0500

    Build #0001
    Added menu framework

---
+++ Assets/Scripts/CellMenuButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMenuButton : MonoBehaviour {
+    public resourceManager gameManager;
+    public Sprite noSelect;
+    public Sprite select;
+    private SpriteRenderer spRend;
+	// Use this for initialization
+	void Start () {
+        spRend = GetComponent<SpriteRenderer>();
+        spRend.sprite = noSelect;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (gameManager.getMenu() == 1)
+        {
+            spRend.sprite = select;
+        }
+        else
+        {
+            spRend.sprite = noSelect;
+        }
+	}
+
+    void OnMouseDown()
+    {
+
+    }
+}
---
+++ Assets/Scripts/resourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resourceManager : MonoBehaviour {
+    public int defaultMenu = 1;
+    private int menu;
+	// Use this for initialization
+	void Start () {
+        menu = defaultMenu;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public int getMenu()
+    {
+        return menu;
+    }
+
+    public void setMenu(int menuToSet)
+    {
+        menu = menuToSet;
+    }
+}
